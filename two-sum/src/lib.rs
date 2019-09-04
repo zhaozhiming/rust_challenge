d3397c8838bc50e3b6f9a9e90178c7aa212ef2cf
zhaozhiming <zhaozhiming003@gmail.com>
2019-09-04 16:27:58 +0800

    feat: add hashmap function to two sum

--- two-sum/src/lib.rs
+++ two-sum/src/lib.rs
@@ -1,4 +1,26 @@
+use std::collections::HashMap;
+
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    // 用hashmap来保存遍历结果，key为数组数字，value为该数字索引
+    let mut map = HashMap::new();
+    for (i, item) in nums.iter().enumerate() {
+        // 组成目标的另外一个数字
+        let rest = target - item;
+        let pre = map.get(&rest);
+        // 如果hashmap没有这个数字则保存，有的话则返回结果
+        if pre.is_some() {
+            // 之前保存的索引
+            let pre_value = pre.unwrap();
+            return vec![*pre_value as i32, i as i32];
+        } else {
+            map.insert(item, i);
+        }
+    }
+
+    panic!("No two sum solution")
+}
+
+pub fn two_sum_violence(nums: Vec<i32>, target: i32) -> Vec<i32> {
     for (i, itemi) in nums[..nums.len() - 1].iter().enumerate() {
         let p = nums.iter().position(|x| x == itemi).unwrap() + 1;
         for (j, itemj) in nums[p..].iter().enumerate() {
--- two-sum/tests/two-sum.rs
+++ two-sum/tests/two-sum.rs
@@ -1,6 +1,19 @@
 use two_sum;
 
 #[test]
-fn test_two_sum() {
+fn test_two_sum_hashmap() {
     assert_eq!(vec![0, 1], two_sum::two_sum(vec![2, 7, 11, 15], 9));
+    assert_eq!(vec![1, 3], two_sum::two_sum(vec![40, 50, 60, 70], 120));
+    assert_eq!(vec![1, 2], two_sum::two_sum(vec![3, 2, 4], 6));
+    assert_eq!(vec![0, 2], two_sum::two_sum(vec![3, 2, 3], 6));
+    assert_eq!(vec![0, 1], two_sum::two_sum(vec![3, 3, 2], 6));
+}
+
+#[test]
+fn test_two_sum_violence() {
+    assert_eq!(vec![0, 1], two_sum::two_sum_violence(vec![2, 7, 11, 15], 9));
+    assert_eq!(vec![1, 3], two_sum::two_sum_violence(vec![40, 50, 60, 70], 120));
+    assert_eq!(vec![1, 2], two_sum::two_sum_violence(vec![3, 2, 4], 6));
+    assert_eq!(vec![0, 2], two_sum::two_sum_violence(vec![3, 2, 3], 6));
+    assert_eq!(vec![0, 1], two_sum::two_sum_violence(vec![3, 3, 2], 6));
 }
